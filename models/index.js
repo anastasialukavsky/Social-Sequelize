54a7d8b5b0faf2d348f03d857261a6c04f93d0f6
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-02 14:15:23 -0400

    define assosiations

--- models/index.js
+++ models/index.js
@@ -1,13 +1,27 @@
-const Comment = require("./Comment");
-const Like = require("./Like");
-const Post = require("./Post");
-const Profile = require("./Profile");
-const User = require("./User");
+const Comment = require('./Comment');
+const Like = require('./Like');
+const Post = require('./Post');
+const Profile = require('./Profile');
+const User = require('./User');
+const UserLike = require('./UserLike');
+
+User.hasOne(Profile);
+Profile.belongsTo(User);
+
+User.hasMany(Post);
+Post.belongsTo(User);
+
+Post.hasMany(Comment);
+Comment.belongsTo(Post);
+
+User.belongsToMany(Like, { through: UserLike });
+Like.belongsToMany(User, { through: UserLike });
 
 module.exports = {
-    Comment,
-    Like,
-    Post,
-    Profile,
-    User
-}
+  Comment,
+  Like,
+  Post,
+  Profile,
+  User,
+  UserLike
+};
