d29fc3bcf71738e2569ca2134028648c19cd1071
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-02 14:14:33 -0400

    add user model

--- models/User.js
+++ models/User.js
@@ -1,5 +1,21 @@
+const db = require('../db/connection')
+const DataTypes = require('sequelize')
 
-let User;
+
+const User = db.define('user', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    isUnique: true,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    },
+  },
+});
 
 
 module.exports = User;
