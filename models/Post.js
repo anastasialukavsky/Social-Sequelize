15c582f31c18a5d075a6fc37a024a4be46b70db9
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-02 14:14:18 -0400

    add post model

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,17 @@
+const db = require('../db/connection');
+const DataTypes = require('sequelize');
 
-let Post;
 
+const Post = db.define('post', {
+  title: {
+    type: DataTypes.STRING,
+  },
+  body: {
+    type: DataTypes.TEXT
+  },
+  cratedAt: {
+    type: DataTypes.DATEONLY
+  }
+});
 
 module.exports = Post;
