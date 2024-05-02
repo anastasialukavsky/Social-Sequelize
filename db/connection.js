104f2a557bbe676be682ceabab14e505756515e4
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-02 14:13:42 -0400

    connect db

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,8 @@
+const { Sequelize } = require('sequelize');
 
+const db = new Sequelize({
+  dialect: 'sqlite',
+  storage: './db.sqlite',
+});
 
-
-
-
-module.exports = {
-    db,
-    
-}
+module.exports = db;
